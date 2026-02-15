b945d7cce58d312ce3bcd35aa704da44e29a10ed
Sukhveer2910 <Sukhveerk986@gmail.com>
2026-02-15 12:24:34 -0600

    Added Welcome screen and update the App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,15 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import WelcomeScreen from "./presentation/screens/WelcomeScreen.vue";
+
+function onWelcomeAction() {
+  console.log("Welcome to the Human Rights Storytelling App!");
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <WelcomeScreen
+    backgroundName="CMHR_exterior.jpg"
+    @startScan="onWelcomeAction"
+    @skipScan="onWelcomeAction"
+  />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
