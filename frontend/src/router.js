076d645e56e21630008361449cc0286d42c720cd
Sukhveer2910 <Sukhveerk986@gmail.com>
2026-02-19 12:52:56 -0600

    Added CaptureScreen and updated it in router too.

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WelcomeScreen from './presentation/screens/WelcomeScreen.vue'
+import CaptureScreen from "./presentation/screens/CaptureScreens.vue";
 import ListScreen from './presentation/screens/ListScreen.vue'
 
 const routes = [
   { path: '/', component: WelcomeScreen },
+  { path: "/capture", component: CaptureScreen },
   { path: '/list', component: ListScreen }
 ]
 
